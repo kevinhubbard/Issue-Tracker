5acd589e7abdce9466911f7283658bed6068bdcb
Kevin <dgkallday98@gmail.com>
2018-05-18 22:45:54 -0400

    compontent css not working as intended

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,16 @@ class IssueFilter extends React.Component{
 
 class IssueRow extends React.Component{
 	render() {
-		const borderedStyle = {border: "1px solid green", padding: 4}
+		const issue = this.props.issue;
 		return(
 				<tr>
-					<td style={borderedStyle}>{this.props.issue_id}</td>
-					<td style={borderedStyle}>{this.props.children}</td>
+					<td>{issue.id}</td>
+					<td>{issue.status}</td>
+					<td>{issue.owner}</td>
+					<td>{issue.created.toDateString()}</td>
+					<td>{issue.effort}</td>
+					<td>{issue.completedDate ? issue.completedDate.toDateString() : ''}</td>
+					<td>{issue.title}</td>
 				</tr>
 			);
 	}
@@ -22,18 +27,22 @@ class IssueRow extends React.Component{
 
 class IssueTable extends React.Component{
 	render() {
-		const borderedStyle = {border: "1px solid silver", padding: 6}
+		const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
 		return(
-				<table style={{borderCollapse: "collapse"}}>
+				<table className='bordered-table'>
 					<thead>
 						<tr>
-							<th style={borderedStyle}>ID</th>
-							<th style={borderedStyle}>Title</th>
+							<th>ID</th>
+							<th>Status</th>
+							<th>Owner</th>
+							<th>Created</th>
+							<th>Effort</th>
+							<th>Completion Date</th>
+							<th>Title</th>
 						</tr>
 					</thead>
 					<tbody>
-						<IssueRow issue_id={1}>Error using this.props.children</IssueRow>
-						<IssueRow issue_id={2}>Missing bottom <b>border</b> on panel.</IssueRow>
+						{issueRows}
 					</tbody>
 				</table>
 			);
@@ -48,6 +57,19 @@ class IssueAdd extends React.Component{
 	}
 }
 
+const issues = [
+	{
+		id: 1, status: 'Open', owner: 'Kevin',
+		created: new Date('2018-05-18'), effort: 5, completedDate: undefined,
+		title: 'Error when clicking add in console.'
+	},
+	{
+		id: 2, status: 'Open', owner: 'Hubbs',
+		created: new Date('2018-05-18'), effort: 1, completedDate: new Date('2018-05-18'),
+		title: 'Missing bottom border.'
+	},
+];
+
 class IssueList extends React.Component {
 	render() {
 		return(
@@ -55,7 +77,7 @@ class IssueList extends React.Component {
 					<h1>Issue Tracker</h1>
 					<IssueFilter />
 					<hr />
-					<IssueTable />
+					<IssueTable issues={issues} />
 					<hr />
 					<IssueAdd />
 				</div>
